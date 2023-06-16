da5ccbc93a3c35224c56382196cf0e355207f3e0
jinyoung1018 <jywsb1816@gmail.com>
2023-06-16 16:52:23 +0900

    사용자 입력 처리

---
+++ src/DiaryEditor.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+
+    const [state, setState] = useState({
+        author: "",
+        content: "",
+    })
+    
+    const [author, setAuthor] = useState("");
+    const [content, setContent] = useState("")
+
+    return(
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+              <input 
+                value={author} 
+                onChange={(e)=>{
+                 setAuthor(e.target.value)
+                }}></input>
+        </div>
+
+        <div>
+            <textarea value={content} onChange={(e)=>{
+                setContent(e.target.value)
+            }}></textarea>
+        </div>
+    </div>
+    );
+
+export default DiaryEditor;
