bfcf5674277b7d42004cb38355a3e169cf716818
jinyoung1018 <jywsb1816@gmail.com>
2023-06-27 16:10:04 +0900

    리스트 아이템 삭제

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from './DiaryList';
 
-// const dummyList = [
-//   {
-//     id:1,
-//     author: "백진영",
-//     content: "하이 1",
-//     emotion:5,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id:2,
-//     author: "박하영",
-//     content: "하이 2",
-//     emotion:3,
-//     created_date: new Date().getTime()
-//   },
-//   {
-//     id:3,
-//     author: "변영무",
-//     content: "하이 4",
-//     emotion:5,
-//     created_date: new Date().getTime()
-//   },
-// ]
-
 function App() {
   const [data, setData] = useState([]);
 
@@ -47,10 +23,16 @@ function App() {
 
   }
 
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다`)
+    const newDiaryList  = data.filter((it) => it.id !== targetId); //id가 targetId와 같지 않은 요소들만 새로운 배열로 반환
+    setData(newDiaryList);
+  }
+
   return (
     <div className="App">
       <DiaryEditor onCreate={onCreate}></DiaryEditor>
-      <DiaryList diaryList={data}></DiaryList>
+      <DiaryList onDelete={onDelete} diaryList={data}></DiaryList>
     </div>
   );
 }
